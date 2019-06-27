8f3ec910563516e9bfebcbfc3b84034e74011423
s15613 <dutaitu92@gmail.cogit config --global user.name s15613git statusqgitgit commit -m 'git test'git commit -m 'git test'git config --global user.name s15613git config --global user.email dutaitu92@gmail.co>
2019-06-27 20:09:57 +0900

    update bla bla

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+require('dotenv').config()
+
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParse = require('body-parser')
 
 const todoRouter = require('./routes')
 
-mongoose.connect('mongodb://localhost/todos', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
         .then(() => console.log("MongoDB successfully connected"))
-  		.catch(err => console.log(err))
+  	.catch(err => console.log(err))
 
 const app = express()
+const port = process.env.PORT || 5000
 
 app.use(bodyParse.json())
 
 app.use('/api/todos',todoRouter)
 
-app.listen(5000, () => console.log('Server is running on port 5000'))
+app.listen(port, () => console.log(`Server is running on port ${port}`))
