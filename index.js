c044f7ce50e0ed1be787dc61663fb5bc5151b8a8
s15613 <dutaitu92@gmail.cogit config --global user.name s15613git statusqgitgit commit -m 'git test'git commit -m 'git test'git config --global user.name s15613git config --global user.email dutaitu92@gmail.co>
2019-06-27 20:14:26 +0900

    deploy to heroku

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParse = require('body-parser')
 
 const todoRouter = require('./routes')
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
         .then(() => console.log("MongoDB successfully connected"))
   	.catch(err => console.log(err))
 
