08afc91c0fc9fece63a7e344226dbdfe112db68a
s15613 <dutaitu92@gmail.cogit config --global user.name s15613git statusqgitgit commit -m 'git test'git commit -m 'git test'git config --global user.name s15613git config --global user.email dutaitu92@gmail.co>
2019-06-28 10:08:10 +0900

    fixed import cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParse = require('body-parser')
+const cors = require('cors')
 
 const todoRouter = require('./routes')
 
