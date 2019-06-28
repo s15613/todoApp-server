794232c10805ddcbf51e0d02223fe157ed12918f
s15613 <dutaitu92@gmail.cogit config --global user.name s15613git statusqgitgit commit -m 'git test'git commit -m 'git test'git config --global user.name s15613git config --global user.email dutaitu92@gmail.co>
2019-06-28 10:05:20 +0900

    added cors

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const port = process.env.PORT || 5000
 
 app.use(bodyParse.json())
 
+app.use(cors())
+
 app.use('/api/todos',todoRouter)
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
